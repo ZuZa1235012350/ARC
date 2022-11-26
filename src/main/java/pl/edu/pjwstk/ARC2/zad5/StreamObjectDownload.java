81f5ac6140a4e6213c49802abc08e113e799dfc1
ZuZa1235012350 <s21243@pjwstk.edu.pl>
2022-11-26 14:14:50 +0100

    Testing if it work without cloud task

---
+++ src/main/java/pl/edu/pjwstk/ARC2/zad5/StreamObjectDownload.java
@@ -0,0 +1,27 @@
+package pl.edu.pjwstk.ARC2.zad5;
+
+
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class StreamObjectDownload {
+
+    public String download() throws IOException {
+
+        try {
+            Storage storage = StorageOptions.getDefaultInstance().getService();
+            BlobId blobId = BlobId.of("arc2-366516.appspot.com", "books.csv");
+            byte[] content = storage.readAllBytes(blobId);
+            String contentString = new String(content, UTF_8);
+            return contentString;
+        }catch (Exception e ){
+            return "Doesn't work :(";
+        }
+
+    }
+}
