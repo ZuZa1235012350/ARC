e6b099cca42779fbcf647db2fd44b0f11d3c8e1a
ZuZa1235012350 <s21243@pjwstk.edu.pl>
2022-11-27 08:13:43 +0100

    Testing if it work without cloud task. Now (?)

--- src/test/java/pl/edu/pjwstk/ARC2/Arc2ApplicationTests.java
+++ src/test/java/pl/edu/pjwstk/ARC2/Arc2ApplicationTests.java
@@ -1,13 +1,12 @@
 package pl.edu.pjwstk.ARC2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Arc2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
